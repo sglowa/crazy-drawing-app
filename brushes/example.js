64e4ee605082841a06609693179f0c8dcbb9e41c
nbriz <nickbriz@gmail.com>
2016-11-02 12:50:42 -0500

    cleaned up post PRs

--- brushes/example.js
+++ brushes/example.js
@@ -1,6 +1,37 @@
+
 function circlePair( x, y, s ){
 
 	ellipse( x-10, y, s, s );
 	ellipse( x+10, y, s, s );
 
 }
+
+
+
+// var myText
+// var i
+// var myFont
+// jenBrush
+
+
+// var steCount
+// stormyNight
+
+// whee
+
+// circles
+
+// var idx
+// bubbleBrush
+
+// pepeBrush
+// var imgBrush
+
+// var abcd
+
+
+
+
+
+
+
