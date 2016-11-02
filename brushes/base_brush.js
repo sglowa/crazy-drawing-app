e2eb6574ad2a4c89f67e9fb75ca1b0492caed090
nbriz <nickbriz@gmail.com>
2016-11-02 14:48:09 -0500

    created base brush class

---
+++ brushes/base_brush.js
@@ -0,0 +1,15 @@
+function Brush(){
+	this.x = 0;
+	this.y = 0;
+	this.size = 0;
+}
+
+Brush.prototype.update = function(x,y){
+	this.x = x;
+	this.y = y;
+}
+
+Brush.prototype.draw = function(x,y){
+
+}
+
